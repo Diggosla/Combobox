689adcf208a8a1274a24d80509b78c1d5e6180fd
Diogo Zanetti <diogozanetti28@gmail.com>
2023-03-03 22:10:35 -0300

    Adicionar arquivos de projeto.

---
+++ Combobox/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Combobox
+{
+    partial class numero
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sair = new System.Windows.Forms.Button();
+            this.buttonnumero = new System.Windows.Forms.Button();
+            this.comboescolha = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelresposta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sair
+            // 
+            this.sair.Location = new System.Drawing.Point(472, 94);
+            this.sair.Name = "sair";
+            this.sair.Size = new System.Drawing.Size(75, 23);
+            this.sair.TabIndex = 0;
+            this.sair.Text = "SAIR";
+            this.sair.UseVisualStyleBackColor = true;
+            // 
+            // buttonnumero
+            // 
+            this.buttonnumero.Location = new System.Drawing.Point(292, 22);
+            this.buttonnumero.Name = "buttonnumero";
+            this.buttonnumero.Size = new System.Drawing.Size(255, 23);
+            this.buttonnumero.TabIndex = 1;
+            this.buttonnumero.Text = "Escolha um numero e clique aqui!";
+            this.buttonnumero.UseVisualStyleBackColor = true;
+            this.buttonnumero.Click += new System.EventHandler(this.buttonnumero_Click);
+            // 
+            // comboescolha
+            // 
+            this.comboescolha.FormattingEnabled = true;
+            this.comboescolha.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12"});
+            this.comboescolha.Location = new System.Drawing.Point(192, 22);
+            this.comboescolha.Name = "comboescolha";
+            this.comboescolha.Size = new System.Drawing.Size(80, 21);
+            this.comboescolha.TabIndex = 2;
+            this.comboescolha.SelectedIndexChanged += new System.EventHandler(this.comboescolha_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Escolha um número e clique 1 a 12:\r\n";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelresposta
+            // 
+            this.labelresposta.AutoSize = true;
+            this.labelresposta.Location = new System.Drawing.Point(34, 56);
+            this.labelresposta.Name = "labelresposta";
+            this.labelresposta.Size = new System.Drawing.Size(0, 13);
+            this.labelresposta.TabIndex = 4;
+            // 
+            // numero
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(554, 129);
+            this.Controls.Add(this.labelresposta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboescolha);
+            this.Controls.Add(this.buttonnumero);
+            this.Controls.Add(this.sair);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "numero";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sair;
+        private System.Windows.Forms.Button buttonnumero;
+        private System.Windows.Forms.ComboBox comboescolha;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelresposta;
+    }
+}
+
---
+++ Combobox/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Combobox
+{
+    public partial class numero : Form
+    {
+        public numero()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonnumero_Click(object sender, EventArgs e)
+        {
+            switch( Convert.ToInt32(comboescolha.SelectedItem))
+            {
+                case 1 :
+                    labelresposta.Text = "O més 1 é equivalente a janeiro";
+                    break;
+
+                    case 2 :
+                    labelresposta.Text = "O mêS 2 é equivalente a Fevareiro";
+                    break;
+
+                    case 3 :
+                    labelresposta.Text = " O mês 3 é equivalente a Março";
+                    break;
+
+                    case 4 :
+                    labelresposta.Text = "O Mês 4 é equivalente a Abril";
+                    break;
+
+                    case 5 :
+                    labelresposta.Text = "O Mês 5 é equivalente a Maio";
+                    break;
+
+                    case 6 :
+                    labelresposta.Text = "O Mês 6 é equivalente a Junho ";
+                    break;
+
+                    case 7 :
+                    labelresposta.Text = "O Mês 7 é equivalente a Julho ";
+                    break;
+
+                    case 8 :
+                    labelresposta.Text = "O Mês 8 é equivalente a  Agosto";
+                    break;
+
+                case 9:
+                    labelresposta.Text = "O Mês 9 é equivalente a Setembro";
+                    break;
+
+                    case 10 :
+                    labelresposta.Text = "O Mês 10 é equivalente a  Outubro";
+                    break;
+
+                    case 11 :
+                    labelresposta.Text = "O Mês 11 é equivalente a Novembro ";
+                    break;
+
+                    case 12 :
+                    labelresposta.Text = " O Mês" + comboescolha.Text +"  é equivalente a Dezembro ";
+                    break;
+
+                    default:
+                    labelresposta.Text = " escolha um nuemro porfavor";
+                    break;
+
+
+            }
+        }
+
+        private void comboescolha_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
